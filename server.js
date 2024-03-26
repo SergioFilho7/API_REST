bf5f414e65831f685b12b933111c3a0b761e34da
SergioFilho7 <sergiobmf2021@gmail.com>
2024-03-26 10:43:39 -0300

    fizemos uma atualização do app.js e server.js

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-import http from "http";
+// import http from "http";
+
+import app from "./src/app.js";
+
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Bem vindo a Rota Inical!",
-    "/livros": "Bem vindo a Rota Livros!",
-    "/autores": "Bem vindo a Rota Autores!"
+// const rotas = {
+//     "/": "Bem vindo a Rota Inical!",
+//     "/livros": "Bem vindo a Rota Livros!",
+//     "/autores": "Bem vindo a Rota Autores!"
 
-}
+// }
 
 // Create a local server to receive data from
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "content.type": "text/plan" });
-    res.end(rotas[req.url]);
-});
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { "content.type": "text/plan" });
+//     res.end(rotas[req.url]);
+// });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Servidor escutando!");
 });
--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 import express from "express";
 const app = express();
+
+app.use(express.json())
+const livros = [
+    {
+        id: 1,
+        titulo: "O Senhor dos Aneís"
+    },
+    {
+        id: 2,
+        titulo: "O Hobbit"
+    }
+]
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+
 app.get("/", (req, res) => {
     res.status(200).send("Rota Home!");
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro Cadastrado com Sucesso!");
 });
 
 export default app;
