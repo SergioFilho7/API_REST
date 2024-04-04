1158f7e09965fcc66b564945dd6c4687d1d681c3
SergioFilho7 <sergiobmf2021@gmail.com>
2024-04-04 11:36:04 -0300

    Finalizado o CRUD no arquivo app.js

--- src/app.js
+++ src/app.js
@@ -13,6 +13,12 @@ const livros = [
     }
 ]
 
+function buscaLivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    });
+}
+
 app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
@@ -24,6 +30,23 @@ app.get("/", (req, res) => {
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("Livro Cadastrado com Sucesso!");
+});
+
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros[index]);
+});
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("Livro Deletado com Sucesso!");
 });
 
 export default app;
